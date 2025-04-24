04a5d75211c9b79754109d5b8c79c1abd1bca6de
Michael Olanrewaju <Michael.Olanrewaju@Sterling.ng>
2025-04-24 19:27:07 +0100

    Initial commit with python exercise

---
+++ main.py
@@ -0,0 +1,23 @@
+# 1. Create a variable called car_name and assign the value lexus to it
+car_name = "lexus"
+
+# 2. Create random integers and sum them
+import random
+
+num_one = random.randint(1, 100)
+num_two = random.randint(1, 100)
+sum_num = num_one + num_two
+
+# 3. Define patient details
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True  # True for new, False for existing
+
+# Print to confirm everything works
+print("Car name:", car_name)
+print("First number:", num_one)
+print("Second number:", num_two)
+print("Sum:", sum_num)
+print("Patient Name:", patient_name)
+print("Patient Age:", patient_age)
+print("Is New Patient:", is_new_patient)
